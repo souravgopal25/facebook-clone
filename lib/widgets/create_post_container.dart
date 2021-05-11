a5b8748476348072d418c22feb9b9a1e75e41182
Sourav Sharma <souravgopal25@gmail.com>
2021-05-11 23:52:52 +0530

    Create Room Widget Complete

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_clone/models/model.dart';
+import 'package:facebook_clone/widgets/widgets.dart';
+
 import 'package:flutter/material.dart';
 
 class CreatePostContainer extends StatelessWidget {
@@ -17,12 +19,7 @@ class CreatePostContainer extends StatelessWidget {
         children: [
           Row(
             children: [
-              CircleAvatar(
-                radius: 20.0,
-                backgroundColor: Colors.grey[200],
-                backgroundImage:
-                    CachedNetworkImageProvider(currentUser.imageUrl),
-              ),
+              ProfileAvatar(imageUrl: currentUser.imageUrl),
               const SizedBox(
                 width: 8.0,
               ),
